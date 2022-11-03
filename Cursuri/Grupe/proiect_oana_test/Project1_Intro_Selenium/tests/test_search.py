0b0f102fe25e5a397696e65dfbfc35a32e3e152d
Gabriela R <98127380+Aktana@users.noreply.github.com>
2022-11-03 22:49:14 +0200

    TAU project

---
+++ Cursuri/Grupe/proiect_oana_test/Project1_Intro_Selenium/tests/test_search.py
@@ -0,0 +1,29 @@
+"""
+These tests cover DuckDuckGo searches.
+"""
+import conftest
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+
+def test_basic_duckduckgo_search(browser):
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  PHRASE = "panda"
+
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
+
+  # When the user searches for "panda"
+  search_page.search(PHRASE)
+
+  # Then the search result title contains "panda"
+  assert PHRASE in result_page.title()
+
+  # And the search result query is "panda"
+  assert PHRASE == result_page.search_input_value()
+
+  # And the search result links pertain to "panda"
+  for title in result_page.result_link_titles():
+    assert PHRASE.lower() in title.lower()
+
